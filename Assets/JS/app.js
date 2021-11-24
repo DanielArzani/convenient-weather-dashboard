8f4c35763aa3583b5b4736f49d913604f6713892
Daniel Arzanipour <daniel.arzanipour@gmail.com>
2021-11-24 16:41:35 -0500

    refactor code and get it working, leave out search bar and storage

--- Assets/JS/app.js
+++ Assets/JS/app.js
@@ -1,15 +1,7 @@
-//todo: CHANGE TIME FORMAT FOR MOMENT.JS AND MAKE IT SO THAT I GET THE TIME FOR THE CITY NAME TYPED IN!
-//todo: GET UV INDEX AND HAVE BACKGROUND CHANGE COLOUR
-//~ Apparently, lon and lat is required for uv background`
-//~ Use geolocation api
-//todo: PERSIST SEARCH HISTORY TO LOCAL STORAGE
-//todo: GET ICON FOR FORECAST
-//todo: CHANGE BACKGROUND COLOUR OF SEARCH HISTORY BUTTONS, THEY'RE TOO DARK
-//todo: CHANGE RESPONSIVENESS FOR CARDS
-//todo: MAKE OPTION FOR CHOOSING CITIES BASED ON COUNTRIES SINCE SOME COUNTRIES HAVE CITIES WITH THE SAME NAME
-//todo: MAKE IT SO THAT THERE ARE NO REPEATS IN SEARCH HISTORY
+//todo: DO I WANT TO SET THE INITIAL DATE TO N/A OR USE MOMENT.JS JUST FOR THAT?
 
 //* VARIABLES
+//^ Search
 // Form
 const form = document.querySelector("form");
 // Form Input
@@ -21,6 +13,7 @@ const apiKey = "39e9d7b8c29775b4c2b37ed1510b744a";
 // Search History
 const searchHistory = document.querySelector("#search-history");
 
+//^ Current Weather
 // Current Date
 const currentDate = document.querySelector("#current-date");
 // City Name in Current Weather Section
@@ -34,6 +27,7 @@ const currentHumidity = document.querySelector(".current-humidity");
 // Current UV Index
 const currentUV = document.querySelector(".current-uv");
 
+//^ Future Weather
 // Future Temp
 const temp = document.querySelectorAll(".temp");
 // Future Wind
@@ -43,108 +37,196 @@ const humidity = document.querySelectorAll(".humidity");
 // Future Dates
 const dates = document.querySelectorAll(".date");
 
+//^ Other
 // Set date
-currentDate.textContent = moment();
+// currentDate.textContent = moment();
+
+// Lon and lat
+let lon;
+let lat;
+
+//^ APIs
+// OneWeatherMap API URL with the city name typed and the api key
+// const weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${apiKey}&units=imperial&current`;
+
+// // Geolocation API
+// const geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${inputValue},&limit=1&appid=${apiKey}`;
+
+// // Weather Forecast for more than one time stamp
+// const forecast = `https://api.openweathermap.org/data/2.5/forecast?q=${inputValue}&appid=${apiKey}&units=imperial&current&cnt=5`;
+
+// const oneCall = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly,alerts&appid=${apiKey}`;
 
 //* FUNCTIONS
 
-// Function that gives weather info
-const weatherData = function (e) {
-  // Prevents site from re-loading upon form submit
-  e.preventDefault();
+//& CURRENT WEATHER
+function weatherData(cityname) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${apiKey}&units=imperial&current`
+  )
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      // Invalid response
+      if (data.cod === "404") {
+        alert("city not found");
+        return;
+      }
 
+      displayWeather(data);
+
+      // Get lat and lon to be able to get uv index
+      let lat = data.coord.lat;
+      let lon = data.coord.lon;
+      fetch(
+        `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&appid=${apiKey}`
+      )
+        .then(function (res) {
+          return res.json();
+        })
+        .then(function (data) {
+          // Sets UV Index
+          currentUV.textContent = data.current.uvi;
+
+          // Changes UV Colour
+          if (currentUV.textContent <= 2) {
+            currentUV.classList.add("favourable");
+          } else if (currentUV.textContent >= 5) {
+            currentUV.classList.add("severe");
+          } else {
+            currentUV.classList.add("moderate");
+          }
+        });
+    });
+
+  // Displays the values in the current weather section (except for uv)
+  function displayWeather(currentWeather) {
+    // Gets the time and displays it
+    const thisDate = new Date(currentWeather.dt * 1000);
+    const day = thisDate.getDate();
+    const month = thisDate.getMonth() + 1;
+    const year = thisDate.getFullYear();
+    currentDate.textContent = `(${month}/${day}/${year})`;
+    // Displays city name
+    currentLocation.textContent = currentWeather.name;
+    // Temp
+    currentTemp.textContent = `Temp: ${currentWeather.main.temp}°F`;
+    // Wind Speed
+    currentWind.textContent = `Wind: ${currentWeather.wind.speed} MPH`;
+    // Humidity
+    currentHumidity.textContent = `Humidity: ${currentWeather.main.humidity} %`;
+  }
+}
+//& END OF CURRENT WEATHER
+
+//& MULTI-DAY WEATHER FORECAST
+// Gets the 5 day forecast
+function multiForecast(cityname) {
+  // Within the data there are 40 timestamps 3 hours apart each, you can pick the days you want from the list
+  fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?q=${cityname}&appid=${apiKey}&units=imperial`
+  )
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      if (data.cod === "404") {
+        alert("city not found");
+        return;
+      }
+      // Calls function that displays the weather values
+      forecastDisplay(data);
+    });
+
+  // Will display the values and icons for all 5 days
+  // Given is a long list and only few of those list items are required so there won't be a loop
+  function forecastDisplay(forecast) {
+    //^ DAY 1
+    // Setting the date
+    const day1Date = new Date(forecast.list[7].dt * 1000);
+    var forecastDay = day1Date.getDate();
+    var forecastMonth = day1Date.getMonth() + 1;
+    var forecastYear = day1Date.getFullYear();
+    dates[0].textContent = `${forecastMonth}/${forecastDay}/${forecastYear}`;
+    //* Setting the icon
+    // Setting the temp
+    temp[0].textContent = `Temp: ${forecast.list[7].main.temp}°F`;
+    // Setting the wind
+    wind[0].textContent = `Wind: ${forecast.list[7].wind.speed}MPH`;
+    // Setting the humidity
+    humidity[0].textContent = `Humidity: ${forecast.list[7].main.humidity} %`;
+    //^ DAY 2
+    // Setting the date
+    const day2Date = new Date(forecast.list[15].dt * 1000);
+    var forecastDay2 = day2Date.getDate();
+    var forecastMonth2 = day2Date.getMonth() + 1;
+    var forecastYear2 = day2Date.getFullYear();
+    dates[1].textContent = `${forecastMonth2}/${forecastDay2}/${forecastYear2}`;
+    //* Setting the icon
+    // Setting the temp
+    temp[1].textContent = `Temp: ${forecast.list[15].main.temp}°F`;
+    // Setting the wind
+    wind[1].textContent = `Wind: ${forecast.list[15].wind.speed}MPH`;
+    // Setting the humidity
+    humidity[1].textContent = `Humidity: ${forecast.list[15].main.humidity} %`;
+    //^ DAY 3
+    // Setting the date
+    const day3Date = new Date(forecast.list[23].dt * 1000);
+    var forecastDay3 = day3Date.getDate();
+    var forecastMonth3 = day3Date.getMonth() + 1;
+    var forecastYear3 = day3Date.getFullYear();
+    dates[2].textContent = `${forecastMonth3}/${forecastDay3}/${forecastYear3}`;
+    //* Setting the icon
+    // Setting the temp
+    temp[2].textContent = `Temp: ${forecast.list[23].main.temp}°F`;
+    // Setting the wind
+    wind[2].textContent = `Wind: ${forecast.list[23].wind.speed}MPH`;
+    // Setting the humidity
+    humidity[2].textContent = `Humidity: ${forecast.list[23].main.humidity} %`;
+    //^ DAY 4
+    // Setting the date
+    const day4Date = new Date(forecast.list[31].dt * 1000);
+    var forecastDay4 = day4Date.getDate();
+    var forecastMonth4 = day4Date.getMonth() + 1;
+    var forecastYear4 = day4Date.getFullYear();
+    dates[3].textContent = `${forecastMonth4}/${forecastDay4}/${forecastYear4}`;
+    //* Setting the icon
+    // Setting the temp
+    temp[3].textContent = `Temp: ${forecast.list[31].main.temp}°F`;
+    // Setting the wind
+    wind[3].textContent = `Wind: ${forecast.list[31].wind.speed}MPH`;
+    // Setting the humidity
+    humidity[3].textContent = `Humidity: ${forecast.list[31].main.humidity} %`;
+    //^ DAY 5
+    // Setting the date
+    const day5Date = new Date(forecast.list[39].dt * 1000);
+    var forecastDay5 = day5Date.getDate();
+    var forecastMonth5 = day5Date.getMonth() + 1;
+    var forecastYear5 = day5Date.getFullYear();
+    dates[4].textContent = `${forecastMonth5}/${forecastDay5}/${forecastYear5}`;
+    //* Setting the icon
+    // Setting the temp
+    temp[4].textContent = `Temp: ${forecast.list[39].main.temp}°F`;
+    // Setting the wind
+    wind[4].textContent = `Wind: ${forecast.list[39].wind.speed}MPH`;
+    // Setting the humidity
+    humidity[4].textContent = `Humidity: ${forecast.list[39].main.humidity} %`;
+  }
+}
+//& END OF MULTI FORECAST
+
+//*EVENT LISTENERS
+
+// When form hear submit event it will call the weatherData function
+form.addEventListener("submit", function (e) {
+  // Prevents page from re-loading
+  e.preventDefault();
   // Variable that stores value of what is submitted through the form
   const inputValue = input.value;
-
-  // OneWeatherMap API URL with the city name typed and the api key
-  const weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${apiKey}&units=imperial&current`;
-
-  // Geolocation API
-  const geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${inputValue},&limit=1&appid=${apiKey}`;
-
-  // Weather Forecast for more than one time stamp
-  const forecast = `https://api.openweathermap.org/data/2.5/forecast?q=${inputValue}&appid=${apiKey}&units=imperial&current&cnt=5`;
-
-  // Lon and lat
-  let lon;
-  let lat;
-
-  const oneCall = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly,alerts&appid=${apiKey}`;
-
-  // Fetch weather data
-
-  //^ FETCH LON AND LAT
-  fetch(geoURL)
-    .then(function (res) {
-      return res.json();
-    })
-    .then(function (data) {
-      // lon = data[0].lon;
-      // lat = data[0].lat;
-      console.log(`Lon: ${data[0].lon} Lat: ${data[0].lat}`);
-    })
-    .catch(function (err) {
-      console.log("err");
-    });
-
-  //^ CURRENT WEATHER
-  fetch(weatherURL)
-    .then(function (res) {
-      return res.json();
-    })
-    .then(function (data) {
-      // console.log(data);
-      // console.log(data.dt * 1000);
-      // console.log(moment().add(24, "hours"));
-      // Adds city name to current weather section
-      currentLocation.textContent = data.name;
-      // Adds city name to search history button
-      const span = document.createElement("span");
-      span.classList.add(
-        "city-name",
-        "py-1",
-        "my-2",
-        "w-100",
-        "btn",
-        "bg-secondary"
-      );
-      span.textContent = data.name;
-      searchHistory.append(span);
-      // Sets TEMP,WIND,HUMIDITY,UV-INDEX values in current weather section
-      currentTemp.textContent = `Temp: ${data.main.temp}°F`;
-      currentWind.textContent = `Wind: ${data.wind.speed} MPH`;
-      currentHumidity.textContent = `Humidity: ${data.main.humidity} %`;
-      // TODO IT SEEMS I NEED LAT AND LON TO GET UV INDEX
-      //   currentUV.textContent = ``;
-    }) //TODO: Get this to display for invalid responses!
-    .catch(function (err) {
-      console.log("ERROR");
-    });
-
-  //^MULTI FORCASTS
-  fetch(oneCall)
-    .then(function (res) {
-      return res.json();
-    })
-    .then(function (data) {
-      // LOOP THROUGH ARRAY AND SET VALUES FOR TEMP WIND AND HUMIDITY
-      data.list.forEach(function (value, index) {
-        //! It seems like you need to pay to get a daily forecast
-        // dates[0].textContent = moment().add(24, `hours`);
-        // dates[1].textContent = moment().add(48, `hours`);
-        // dates[2].textContent = moment().add(72, `hours`);
-        // dates[3].textContent = moment().add(96, `hours`);
-        // dates[4].textContent = moment().add(120, `hours`);
-        temp[index].textContent = `Temp: ${value.main.temp}°F`;
-        wind[index].textContent = `Wind:${value.wind.speed}MPH`;
-        humidity[index].textContent = `Humidity: ${value.main.humidity} %`;
-      });
-    })
-    .catch(function (err) {
-      console.log(err);
-    });
-};
-
-//* EVENT LISTENERS
-// When form hear submit event it will call the weatherData function
-form.addEventListener("submit", weatherData);
+  // Call current weather function
+  weatherData(inputValue);
+  // Call several weather forecasts
+  multiForecast(inputValue);
+});
